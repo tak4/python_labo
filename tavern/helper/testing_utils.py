7359caea8c17ff033cb465cfdcbcbf6a7bdc88b7
tak4 <tak.shi.4@gmail.com>
2024-02-28 23:18:44 +0900

    modify

--- tavern/helper/testing_utils.py
+++ tavern/helper/testing_utils.py
@@ -1,3 +1,11 @@
+import json
+
 def is_specified_json(response, schema):
     json = response.json()
     assert json.get("item") == schema.get("item")
+
+def save_response(response):
+    j = response.json()
+    with open("data.json", "w") as f:
+        # JSONデータを書き出す
+        json.dump(j, f)
