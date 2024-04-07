63ec4802a31663948912ee96c6db96cf84024f7c
tak4 <tak.shi.4@gmail.com>
2024-04-07 18:54:16 +0900

    レスポンスをsaveディレクトリへ保存する Tavern変数保存のサンプル作成

--- tavern/helper/testing_utils.py
+++ tavern/helper/testing_utils.py
@@ -1,4 +1,7 @@
+import os
 import json
+
+SAVE_DIR = f"./save/"
 
 def is_specified_json(response, schema):
     json = response.json()
@@ -6,6 +9,18 @@ def is_specified_json(response, schema):
 
 def save_response(response, fname):
     j = response.json()
-    with open(fname, "w") as f:
+
+    if not os.path.isdir(SAVE_DIR):
+        os.mkdir(SAVE_DIR)
+
+    with open(SAVE_DIR + fname, "w") as f:
         # JSONデータを書き出す
         json.dump(j, f)
+
+def disp_response(response, _id):
+
+    if not os.path.isdir(SAVE_DIR):
+        os.mkdir(SAVE_DIR)
+
+    with open(f"./save/_id.txt", "w") as f:
+        f.write(_id)
