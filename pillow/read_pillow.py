5037e6f26436e008db079e0dc8dd686a0f157167
tak4 <tak.shi.4@gmail.com>
2024-05-02 04:49:24 +0900

    update

--- pillow/read_pillow.py
+++ pillow/read_pillow.py
@@ -3,21 +3,33 @@ from PIL import Image
 
 im = Image.open("./img/R.bmp")
 
-array = np.array(im)
+array = np.array(im, dtype=np.int32)
 
 np.set_printoptions(threshold=32*32*3)
+print(np.shape(array), type(im))
 print(array)
-print(np.shape(array))
 
 np.savetxt('./output/np_save_bmp_R.csv', array, delimiter=',', fmt='%x')
 
 
 im = Image.open("./img/R_red.bmp")
 
-array = np.array(im)
+array = np.array(im, dtype=np.int32)
 
 np.set_printoptions(threshold=32*32*3)
+print(np.shape(array), type(im))
 print(array)
-print(np.shape(array))
 
-np.savetxt('./output/np_save_bmp_R_red.csv', array, delimiter=',', fmt='%x')
+im = Image.open("./img/R_red.bmp")
+im.convert("RGB")
+
+pixelSizeTuple = im.size
+
+plist = []
+
+for i in range(pixelSizeTuple[0]):
+	for j in range(pixelSizeTuple[1]):
+		p = im.getpixel((i,j))
+		plist.append(p)
+		
+print(plist)
