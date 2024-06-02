592d85d28cd24027cc0c30a3fe83f8fa3c921e39
tak4 <tak.shi.4@gmail.com>
2024-06-02 11:02:32 +0900

    modify

---
+++ Azure/blob_storage/download_azure_blob_storage.py
@@ -0,0 +1,54 @@
+import os
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+# 初期設定
+# TODO: Replace <storage-account-name> with your actual storage account name
+account_url = "https://<storage-account-name>.blob.core.windows.net"
+# Storage Access Key
+shared_access_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
+# 接続先文字列
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+# コンテナ名
+container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+
+# Create a local directory to hold blob data
+download_path = "./download"
+if not os.path.exists(download_path):
+    os.mkdir(download_path)
+
+# ２．接続先文字列を用いて BlobServiceClient を作成する
+# 予め環境変数 AZURE_STORAGE_CONNECTION_STRING に接続文字列を設定しておく
+# Linuxの場合
+# export AZURE_STORAGE_CONNECTION_STRING=""
+# 接続文字列は、ストレージアカウント＞アクセスキー で取得する
+def get_blob_service_client_connection_string():
+
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    return blob_service_client
+
+# 接続文字列から、BlobServiceClient object を生成
+blob_service_client = get_blob_service_client_connection_string()
+
+
+# List the blobs in the container
+print("\nListing blobs...")
+
+container_client = blob_service_client.get_container_client(container=container_name) 
+blob_list = container_client.list_blobs()
+latest_blob = None
+for blob in blob_list:
+    print("\t" + blob.name, blob.last_modified)
+    if latest_blob is None or blob.last_modified > latest_blob.last_modified:
+        latest_blob = blob
+
+print("latest_blob.name : " + latest_blob.name)
+
+# Download the blob to a local file
+# Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+download_file_path = os.path.join(download_path, str.replace(latest_blob.name ,'.txt', '_DOWNLOAD.txt'))
+print("\nDownloading blob to \n\t" + download_file_path)
+
+with open(file=download_file_path, mode="wb") as download_file:
+    download_file.write(container_client.download_blob(blob.name).readall())
--- Azure/blob_storage/upload_azure_blob_storage.py
+++ Azure/blob_storage/upload_azure_blob_storage.py
@@ -1,3 +1,4 @@
+# Azure Blob Storage に対してファイルをアップロード／ダウンロードする
 # 参考ページ
 # https://learn.microsoft.com/ja-jp/azure/storage/blobs/storage-quickstart-blobs-python?tabs=managed-identity%2Croles-azure-portal%2Csign-in-azure-cli&pivots=blob-storage-quickstart-scratch
 
@@ -6,48 +7,55 @@ import uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
+# 初期設定
+# TODO: Replace <storage-account-name> with your actual storage account name
+account_url = "https://<storage-account-name>.blob.core.windows.net"
+# Storage Access Key
+shared_access_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
+# 接続先文字列
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+# コンテナ名
+container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
 
+# １．クレデンシャル情報を用いて BlobServiceClient を作成する
 def get_blob_service_client_account_key():
-    # TODO: Replace <storage-account-name> with your actual storage account name
-    account_url = "https://<storage-account-name>.blob.core.windows.net"
-    shared_access_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
-    credential = shared_access_key
 
     # Create the BlobServiceClient object
+    credential = shared_access_key
     blob_service_client = BlobServiceClient(account_url, credential=credential)
 
     return blob_service_client
 
 
+# ２．接続先文字列を用いて BlobServiceClient を作成する
 # 予め環境変数 AZURE_STORAGE_CONNECTION_STRING に接続文字列を設定しておく
 # Linuxの場合
 # export AZURE_STORAGE_CONNECTION_STRING=""
 # 接続文字列は、ストレージアカウント＞アクセスキー で取得する
 def get_blob_service_client_connection_string():
-    # TODO: Replace <storage-account-name> with your actual storage account name
-    account_url = "https://<storage-account-name>.blob.core.windows.net"
-    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
     # Create the BlobServiceClient object
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     return blob_service_client
 
+
 # 接続文字列から、BlobServiceClient object を生成
 blob_service_client = get_blob_service_client_connection_string()
 
-# コンテナ名
-container_name = "imageanalysis"
-
 # Create a local directory to hold blob data
-local_path = "./data"
-if not os.path.exists(local_path):
-    os.mkdir(local_path)
+upload_path = "./upload"
+if not os.path.exists(upload_path):
+    os.mkdir(upload_path)
+
+download_path = "./download"
+if not os.path.exists(download_path):
+    os.mkdir(download_path)
 
 try:
     # Create a file in the local data directory to upload and download
     local_file_name = str(uuid.uuid4()) + ".txt"
-    upload_file_path = os.path.join(local_path, local_file_name)
+    upload_file_path = os.path.join(upload_path, local_file_name)
 
     # Write text to the file
     file = open(file=upload_file_path, mode='w')
@@ -65,31 +73,9 @@ try:
 
 except Exception as e:
     # その他の例外が発生した場合の処理
-    print(f"予期しないエラーが発生しました: {e}")
+    print(f"An unexpected error has occurred: {e}")
 
 finally:
     # 必ず実行される処理
-    print("処理が完了しました")
+    print("Processing completed")
 
-print("\nListing blobs...")
-
-
-# List the blobs in the container
-container_client = blob_service_client.get_container_client(container=container_name) 
-blob_list = container_client.list_blobs()
-latest_blob = None
-for blob in blob_list:
-    print("\t" + blob.name, blob.last_modified)
-    if latest_blob is None or blob.last_modified > latest_blob.last_modified:
-        latest_blob = blob
-
-print("latest_blob.name : " + latest_blob.name)
-
-# Download the blob to a local file
-# Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
-download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
-container_client = blob_service_client.get_container_client(container=container_name) 
-print("\nDownloading blob to \n\t" + download_file_path)
-
-with open(file=download_file_path, mode="wb") as download_file:
-    download_file.write(container_client.download_blob(blob.name).readall())
