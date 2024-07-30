ce6985ad66780ab6d7804fcf0dbcac18687b8648
tak4 <tak.shi.4@gmail.com>
2024-07-31 05:50:27 +0900

    modify json

--- json/json_test.py
+++ json/json_test.py
@@ -8,26 +8,34 @@ country_capitals = {
   "Japan" : {"Tokyo": 2194}
 }
 
-# dictionay to json file
+# dictionary
+print('--- dictionary ---')
+print(country_capitals, type(country_capitals))
+
+# dictionay to string
+s = json.dumps(country_capitals, indent=4)
+print('--- dictionay to json string ---')
+print(s, type(s))
+
+# string to dictionary
+# json.loads() は、文字列を引数として取る
+data = json.loads(s)
+print('--- string to dictionary ---')
+print(data, type(data))
+
+
+# dictionary to file(file)
+print('--- dictionary to file(file) ---')
 with open('./output/country_capitals.json', 'w') as f:
     json.dump(country_capitals, f, indent=4)
-
-# dictionay to json string
-s = json.dumps(country_capitals, indent=4)
-print(s, type(s))
 
 # with open('./output/country_capitals_shape.json', 'w') as f:
 #     print(country_capitals)
 
-# json.load() と json.loads() は、どちらも JSON 形式のデータを Python オブジェクトに変換する関数
+# file(json) to dictionary
 # json.load() は、ファイルオブジェクトを引数として取る
 with open('./output/country_capitals.json', 'r') as f:
     data = json.load(f)
-
-# Pythonオブジェクトを出力
-print(data)
-
-# json.loads() は、文字列を引数として取る
-data = json.loads(s)
-print(s)
+print('--- file(file) to dictionary ---')
+print(data, type(data))
 
