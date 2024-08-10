19fd14284c5ec33d45551650b5d5f1440d72b5dc
tak4 <tak.shi.4@gmail.com>
2024-08-10 17:09:32 +0900

    modify codestyle

---
+++ codestyle/cstyle/cstyle.py
@@ -0,0 +1,5 @@
+import datetime
+
+def add(a, b):
+    return a + b
+
--- codestyle/sample.py
+++ codestyle/sample.py
@@ -1,2 +1,18 @@
+import datetime
+
+import cstyle
+import cstyle.cstyle
+
 a1 = {'key1', 'value1'}
 a2 = {'key2',             'value2'}
+
+array1 = [ (x, y) for x in range(10) for y in range(10) ]
+print(array1)
+
+array2 = [ x for x in range(10) if x % 2 == 0 ]
+print(array2)
+
+sum = cstyle.cstyle.add(10, 20)
+print(sum)
+
+print(datetime.datetime.utcnow())
