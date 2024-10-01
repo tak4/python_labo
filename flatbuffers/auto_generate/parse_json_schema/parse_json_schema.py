a3510bf7b2620397007368f526f132491253b871
tak4 <tak.shi.4@gmail.com>
2024-10-02 00:19:10 +0900

    modify

--- flatbuffers/auto_generate/parse_json_schema/parse_json_schema.py
+++ flatbuffers/auto_generate/parse_json_schema/parse_json_schema.py
@@ -2,33 +2,52 @@ import json
 import os
 import pprint
 import sys
+import yaml
 base_path = os.path.dirname(
                     os.path.dirname(
                         os.path.abspath(__file__)))
 sys.path.append(base_path)
-from SmartCamera import BoundingBox
-from SmartCamera import BoundingBox2d
-from SmartCamera import ObjectDetectionTop
-import SmartCamera
-
 
 with open('../json/jsonschema/objectdetection.schema.json', 'r') as f:
     json_schema = json.load(f)
 
 # pprint.pprint(json_schema, indent=2)
 
-for k, v in json_schema['definitions'].items():
-    definition = k.split('_')
-    print(definition[-1])
-    if 'type' in v:
-        type = v['type']
-        print(type)
+def resolve_refs(schema, definitions):
+    '''flatbuffrers jsonschemaのrefを解決して階層構造に展開する
+    Args:
+        schema schema
+        definitions definitions以下の要素
+    '''
 
-    if 'properties' in v:
-        for property, property_v in v['properties'].items():
-            print(property)
-            if 'type' in property_v:
-                property_type = property_v['type']
-                print(property_type)
+    if isinstance(schema, dict):
+        if '$ref' in schema:
+            ref_path = schema['$ref'].split('/')
+            ref_key = ref_path[-1]
+            return {ref_key: resolve_refs(definitions[ref_key], definitions)}
+        else:
+            return {k: resolve_refs(v, definitions) for k, v in schema.items()}
+    elif isinstance(schema, list):
+        return [resolve_refs(item, definitions) for item in schema]
+    else:
+        # 末端の要素
+        return schema
+
+resolved_schema = resolve_refs(json_schema, json_schema.get('definitions', {}))
+
+with open('output.yaml', 'w') as yaml_file:
+    yaml.dump(resolved_schema, yaml_file, default_flow_style=False, allow_unicode=True)
 
 
+def recursive(schema):
+    if isinstance(schema, dict):
+        for k, v in schema.items():
+            if 'properties' == k:
+                pass
+            elif 'items' == v:
+                print(f'{k}(list)')
+            else:
+                print(k)
+            recursive(v)
+
+recursive(resolved_schema)
