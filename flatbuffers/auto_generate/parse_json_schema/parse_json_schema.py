a52430e8e8f13036f246d3150235307507c9d9d2
tak4 <tak.shi.4@gmail.com>
2024-10-07 00:59:42 +0900

    modify

--- flatbuffers/auto_generate/parse_json_schema/parse_json_schema.py
+++ flatbuffers/auto_generate/parse_json_schema/parse_json_schema.py
@@ -34,41 +34,89 @@ def resolve_refs(schema, definitions):
         # keyがlistであれば、listを展開する
         return [resolve_refs(item, definitions) for item in schema]
     else:
-        # keyがdictかlistでなく、末端の要素の場合は再帰しない
+        # keyがdictかlistでなく、末端の要素の場合は再帰せず、そのまま返す
         return schema
+
+def get_object_value(schema, key):
+    ret_value = None
+    # object内を走査する
+    if isinstance(schema, dict):
+        for k, v in schema.items():
+            if key == k:
+                ret_value = v
+                break
+    return ret_value
+
+def snake_to_camel(snake_str):
+    components = snake_str.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+stack = deque()
+def recursive(schema = None, parent = None, indent = 0):
+    if parent is not None:
+        stack.append(parent)
+
+    indent = len(stack)
+
+    if isinstance(schema, dict):
+        for k, v in schema.items():
+            obj_type = get_object_value(schema[k], "type")
+            if "object" == obj_type:
+                if parent is None:
+                    # root_typeの処理
+                    module_names = k.split('_')
+                    print(f'elem{indent} = ', end="")
+                    print('.'.join(module_names[1:]) + '.', end="")
+                    print(f'GetRootAs{module_names[-1]}(buf, 0)')
+                obj_properties = get_object_value(schema[k], "properties")
+                recursive(obj_properties, k, len(stack))
+            if "array" == obj_type:
+                obj_items = get_object_value(schema[k], "items")
+                print('  ' * indent + f'elem{indent} = elem{indent-1}.{snake_to_camel(k)}()')
+                recursive(obj_items, k, len(stack))
+            elif "number" == obj_type or "string" == obj_type:
+                print(k, v)
+            elif obj_type == None:
+                print('  ' * indent + f'elem{indent} = elem{indent-1}.{k.title()}()')
+                recursive(v, k, len(stack))
+
+            # if 'type' == k:
+            #     print('  ' * indent + f'{stack[-1]} -> {k}: {v}')
+            #     if indent > 0:
+            #         print('  ' * indent + f'elem{indent} = elem{indent-1}.{stack[-1]}()')
+
+            #     # ネストを1段下げて再帰
+            #     stack.append(k)
+            #     recursive(v, len(stack))
+            #     stack.pop()
+            # elif 'enum' == k:
+            #     print('  ' * indent + f'{stack[-1]} -> {k}: {v}')
+            # elif 'additionalProperties' == k:
+            #     pass
+            # elif 'properties' == k:
+            #     recursive(v, len(stack))
+            # else:
+            #     if indent == 0:
+            #         # root_typeの処理
+            #         module_names = k.split('_')
+            #         print(f'elem{indent} = ', end="")
+            #         print('.'.join(module_names[1:]) + '.', end="")
+            #         print(f'GetRootAs{module_names[-1]}(buf, 0)')
+            #     # ネストを1段下げて再帰
+            #     stack.append(k)
+            #     recursive(v, len(stack))
+            #     stack.pop()
+    elif isinstance(schema, list):
+        for l in schema:
+            recursive(l, stack[-1], indent)
+    if parent is not None:
+        stack.pop()
 
 resolved_schema = resolve_refs(json_schema, json_schema.get('definitions', {}))
 
 with open('output.yaml', 'w') as yaml_file:
     yaml.dump(resolved_schema, yaml_file, default_flow_style=False, allow_unicode=True)
 
-
-stack = deque()
-
-def recursive(schema, indent):
-    if isinstance(schema, dict):
-        for k, v in schema.items():
-            
-            if 'type' == k or 'enum' == k or 'additionalProperties' == k:
-                print('  ' * indent + f'{k}: {v}')
-            else:
-                print('  ' * indent + k)
-            # if 'properties' == k:
-            #     pass
-            # elif 'type' == k:
-            #     print(f'{k}: {v}')
-            # else:
-            #     print(k)
-            
-            stack.append(k)
-            recursive(v, len(stack))
-            stack.pop()
-    elif isinstance(schema, list):
-        for l in schema:
-            recursive(l, indent)
-
-
-
-recursive(resolved_schema, 0)
+recursive(schema=resolved_schema, parent=None, indent=0)
 
 print(stack)
