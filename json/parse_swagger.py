34c7d1dc4a0d4350662ff61b55edad15e51d6f75
tak4 <tak.shi.4@gmail.com>
2024-10-22 00:19:15 +0900

    modify

--- json/parse_swagger.py
+++ json/parse_swagger.py
@@ -4,6 +4,9 @@ import sys
 import json
 
 KEY_SUMMARY = 'summary'
+KEY_DESCRIPTION = 'description'
+KEY_TAGS = 'tags'
+KEY_OPERATIONID = 'operationId'
 KEY_PARAMETERS = 'parameters'
 KEY_REQUEST_BODY = 'requestBody'
 KEY_RESPOMSES = 'responses'
@@ -13,57 +16,115 @@ KEY_MILTIPART_FORM_DATA = 'multipart/form-data'
 
 target_summary = ''
 
-# ファイル名取得
-args = sys.argv
-if 1 == len(args):
-    print("Usage " + args[0] + " swagger.json")
-    sys.exit()
 
-# jsonファイル読み込み
-json_file_name = sys.argv[1]
-with open(json_file_name, 'r') as f:
-    data = json.load(f)
+nest_count = 0
+def recursive_ref(nc, data, d):
+    if isinstance(d, dict):
+        for k, v in d.items():
+            if '$ref' == k:
+                ref_list = v.split('/')
+                data_contents = data[ref_list[1]][ref_list[2]][ref_list[3]]
+                recursive_ref(nc + 1, data, data_contents)
+            else:
+                print('    ' * nc + '{:<15}: {}'.format(k, v))
+                recursive_ref(nc + 1, data, v)
+    # else:
+    #     print(d)
 
-# paths を取得
-data_paths = data['paths']
-data_paths_keys = data_paths.keys()
+def main():
 
-# pathsの中身を取得
-for path in data_paths_keys:
+    # ファイル名取得
+    args = sys.argv
+    if 1 == len(args):
+        print("Usage " + args[0] + " swagger.json")
+        sys.exit()
 
-    data_path = data_paths[path]
-    req_methods = data_path.keys()
+    # jsonファイル読み込み
+    json_file_name = sys.argv[1]
+    with open(json_file_name, 'r') as f:
+        data = json.load(f)
 
-    # method get/put/delete 等でまわす
-    for rm in req_methods:
-        summary = data_path[rm][KEY_SUMMARY]
-        if target_summary == '' or summary == target_summary:
-            print(f'##### {summary}')
+    # paths を取得
+    data_paths = data['paths']
+    data_paths_keys = data_paths.keys()
 
-            # parameters
-            if KEY_PARAMETERS in data_path[rm]:
-                parameters = data_path[rm][KEY_PARAMETERS]
-                print(f'----- {KEY_PARAMETERS}')
-                for param in parameters:
-                    pprint.pprint(param)
+    set_req_method_keys = set()
 
-            # requestBody
-            if KEY_REQUEST_BODY in data_path[rm]:
-                responses = data_path[rm][KEY_REQUEST_BODY]['content']
-                print(f'----- {KEY_REQUEST_BODY}')
-                for response in responses.keys():
-                    print(response)
-                    if '$ref' in responses[response]['schema']:
-                        ref = responses[response]['schema']['$ref']
-                        ref_list = ref.split('/')
-                        data_contents = data[ref_list[1]][ref_list[2]][ref_list[3]]
-                        pprint.pprint(data_contents)
+    # pathsの中身を取得
+    for path in data_paths_keys:
 
-            # responses
-            if KEY_RESPOMSES in data_path[rm]:
-                responses = data_path[rm][KEY_RESPOMSES]
-                print(f'----- {KEY_RESPOMSES}')
-                for response in responses:
-                    pprint.pprint(responses[response])
+        data_path = data_paths[path]
+        req_methods = data_path.keys()
 
-            print()
+        # method get/put/delete 等でまわす
+        for rm in req_methods:
+            summary = data_path[rm][KEY_SUMMARY]
+            if target_summary == '' or summary == target_summary:
+                print(f'# {summary}')
+                print('----- path')
+                print(path)
+                print('----- method')
+                print(f'{rm}')
+
+                # keyの種類チェック用
+                # for k in data_path[rm].keys():
+                #     set_req_method_keys.add(k)
+
+                # description
+                if KEY_DESCRIPTION in data_path[rm]:
+                    desctiption = data_path[rm][KEY_DESCRIPTION]
+                    print(f'----- {KEY_DESCRIPTION}')
+                    print(desctiption)
+
+                # keys
+                if KEY_TAGS in data_path[rm]:
+                    tags = data_path[rm][KEY_TAGS]
+                    print(f'----- {KEY_TAGS}')
+                    print(tags)
+
+                if KEY_OPERATIONID in data_path[rm]:
+                    opeid = data_path[rm][KEY_OPERATIONID]
+                    print(f'----- {KEY_OPERATIONID}')
+                    print(opeid)
+
+                # parameters
+                if KEY_PARAMETERS in data_path[rm]:
+                    parameters = data_path[rm][KEY_PARAMETERS]
+                    print(f'----- {KEY_PARAMETERS}')
+                    for params in parameters:
+                        for k, v in params.items():
+                            print('{:<15}: {}'.format(k, v))
+
+                # requestBody
+                if KEY_REQUEST_BODY in data_path[rm]:
+                    responses = data_path[rm][KEY_REQUEST_BODY]['content']
+                    print(f'----- {KEY_REQUEST_BODY}')
+                    recursive_ref(0, data, responses)
+                    # for response in responses.keys():
+                    #     print(response)
+                        # if '$ref' in responses[response]['schema']:
+                        #     ref = responses[response]['schema']['$ref']
+                        #     ref_list = ref.split('/')
+                        #     data_contents = data[ref_list[1]][ref_list[2]][ref_list[3]]
+                        #     for k, v in data_contents.items():
+                        #         print('{:<15}: {}'.format(k, v))
+
+
+                # responses
+                if KEY_RESPOMSES in data_path[rm]:
+                    responses = data_path[rm][KEY_RESPOMSES]
+                    print(f'----- {KEY_RESPOMSES}')
+                    recursive_ref(0, data, responses)
+                    # for response in responses:
+                    #     pprint.pprint(responses[response])
+
+                print()
+
+
+    # keyの種類チェック用
+    for s in set_req_method_keys:
+        print(s)
+
+
+if __name__ == '__main__':
+    main()
