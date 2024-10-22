9d2a5d000d4f6c478fe2e5cbe50d30b570e7e188
tak4 <tak.shi.4@gmail.com>
2024-10-23 00:11:36 +0900

    modify

--- flatbuffers/auto_generate/flatc_test/all_generate.sh
+++ flatbuffers/auto_generate/flatc_test/all_generate.sh
@@ -21,6 +21,24 @@ sample16
 sample17
 sample18
 sample19
+sample20
+sample21
+sample100
+sample101
+sample102
+sample103
+sample104
+sample105
+sample106
+sample107
+sample108
+sample109
+sample110
+sample111
+sample112
+sample113
+sample114
+sample115
 )
 
 # 配列の要素をループで取り出す
