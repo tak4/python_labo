c48a00862c4eb7db531b7975e52012838b8aac88
tak4 <tak.shi.4@gmail.com>
2025-10-25 16:51:42 +0900

    modify

--- file_operation/copyfile_by_csvfile_list/copyfile_by_csvfile_list.py
+++ file_operation/copyfile_by_csvfile_list/copyfile_by_csvfile_list.py
@@ -2,16 +2,21 @@ import csv
 import os
 import shutil
 
-with open('./copyfile_list_rusult.txt', 'w') as output_file:
-    with open('./copyfile_list.csv', 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            source_file = os.path.join(row['source_path'], row['source_file'])
-            # 空文字／空白のみなら source_file 名を使う
-            target_basename = row.get('target_file', '').strip() or row['source_file']
-            target_file = os.path.join(
-                row['target_path'], 
-                target_basename
-            )
-            output_file.write('{}\t{}\n'.format(source_file, target_file))
-            shutil.copy2(source_file, target_file)
+
+def main():
+    with open('./copyfile_list_rusult.txt', 'w') as output_file:
+        with open('./copyfile_list.csv', 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                source_file = os.path.join(row['source_path'], row['source_file'])
+                # 空文字／空白のみなら source_file 名を使う
+                target_basename = row.get('target_file', '').strip() or row['source_file']
+                target_file = os.path.join(
+                    row['target_path'], 
+                    target_basename
+                )
+                output_file.write('{}\t{}\n'.format(source_file, target_file))
+                shutil.copy2(source_file, target_file)
+
+if __name__ == "__main__":
+    main()
