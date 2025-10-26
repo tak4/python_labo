ba13f92e1ee4e2f2afd9fef70faec45f39fc3e38
tak4 <tak.shi.4@gmail.com>
2025-10-26 17:23:51 +0900

    modify

--- file_operation/find_string/find_string.py
+++ file_operation/find_string/find_string.py
@@ -1,48 +1,35 @@
 import csv
 import re
+from optparse import OptionParser
 from pathlib import Path
 
 
 def main():
 
+    # パラメータ指定の準備
+    usage = 'usage: %prog [options]'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-t', '--target', action='store', type='string',
+                      dest='target', help='target directory')
+
+    # パラメータの取得
+    options, args = parser.parse_args()
+    print(options.target)
+
     # 検索する単語のリストを作成する
     search_word_list = make_search_word_list('keyword_list.csv')
 
-    # Windowsでは、open関数を使ってファイルを開くと、
-    # デフォルトでテキストモードになり、改行コードが自動的に変換される
-    # その為、newline=""を指定しておく
-    with open('keyword_list_result.csv', 'w', newline="") as csvfile:
-        # Header作成
-        fieldnames = ['file', 'line_no', 'line_string', 'word']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        # 指定ディレクトリ以下、指定拡張子のファイルを選択して、単語リストの単語を検索する
-        p = Path('./target')
-        target_file_list = [f for pat in ('*.txt', '*.md', '*.log') for f in p.rglob(pat)]
-        for target_file in target_file_list:
-            # 1ファイルごとに単語リストの単語を検索する
-            result_list = search_words_output(target_file, search_word_list)
-
-            # csv検索結果をcsvファイルへ出力する
-            for result in result_list:
-                writer.writerow(
-                    {
-                        fieldnames[0]: result[0],
-                        fieldnames[1]: result[1],
-                        fieldnames[2]: result[2],
-                        fieldnames[3]: result[3]
-                    }
-                )
-                print(fieldnames, result)
+    # 単語のリストを元に指定ディレクトリしたのファイルを検索して結果をcsvファイルに出力する
+    create_search_result(options.target if options.target is not None else 'target', search_word_list)
 
 
 def make_search_word_list(keyword_list_file_name):
-    """csvファイルから検索する単語のリストを作成する
+    """csvファイルに記載されている検索する単語をリスト化する
     Args:
         csv_file_name (string) : 検索する単語が記述されたcsvファイル
     Returns:
-        search_word_list (string) : 検索する単語のリスト ※正規表現はコンパイル済みオブジェクト
+        search_word_list (string) : 検索する単語のリスト
+        ※正規表現はコンパイル済みオブジェクトを格納する
     """
 
     search_word_list = []
@@ -62,8 +49,41 @@ def make_search_word_list(keyword_list_file_name):
     return search_word_list
 
 
-def search_words_output(file_name, search_word_list):
-    """ファイルを複数ワードで検索して一致した行を出力する
+def create_search_result(target, search_word_list):
+    """単語リストのの単語を指定ファイル内で検索し、結果をcsvファイルに出力する
+    """
+
+    # Windowsでは、open関数を使ってファイルを開くと、
+    # デフォルトでテキストモードになり、改行コードが自動的に変換される
+    # その為、newline=""を指定しておく
+    with open('keyword_list_result.csv', 'w', newline="") as csvfile:
+        # Header作成
+        fieldnames = ['file', 'line_no', 'line_string', 'word']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # 指定ディレクトリ以下、指定拡張子のファイルを選択して、単語リストの単語を検索する
+        p = Path(target)
+        target_file_list = [f for pat in ('*.txt', '*.md', '*.log') for f in p.rglob(pat)]
+        for target_file in target_file_list:
+            # 1ファイルごとに単語リストの単語を検索する
+            result_list = search_words(target_file, search_word_list)
+
+            # 単語の検索結果をcsvファイルへ出力する
+            for result in result_list:
+                writer.writerow(
+                    {
+                        fieldnames[0]: result[0],
+                        fieldnames[1]: result[1],
+                        fieldnames[2]: result[2],
+                        fieldnames[3]: result[3]
+                    }
+                )
+                print(fieldnames, result)
+
+
+def search_words(file_name, search_word_list):
+    """検索対象のファイルをリストで指定された単語で検索して一致した行をリストへ出力する
     Args:
         file_name (string) : 検索対象を行うファイル名
         search_word_list (list) : 検索文字列のリスト ※正規表現はcompile済みオブジェクト
@@ -86,6 +106,7 @@ def search_words_output(file_name, search_word_list):
                         search_result_list.append((str(file_name), line_no+1, line_string, str(word))) 
 
     return search_result_list
+
 
 if __name__ == "__main__":
     main()
