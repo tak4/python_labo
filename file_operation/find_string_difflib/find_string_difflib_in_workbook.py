ea1ce8f339ea334776b0c3cb503a8f456cbb8c19
tak4 <tak.shi.4@gmail.com>
2026-09-10 06:00:53 +0900

    modify

--- file_operation/find_string_difflib/find_string_difflib_in_workbook.py
+++ file_operation/find_string_difflib/find_string_difflib_in_workbook.py
@@ -2,6 +2,14 @@ from difflib import SequenceMatcher
 import openpyxl
 
 
+def check_similarity_threshold(a: str, b: str, ratio_threshold: float = 0.5) -> tuple[bool, float]:
+    over_threshold = False
+    ratio_result = SequenceMatcher(None, a, b).ratio()
+    if ratio_result >= ratio_threshold:
+        over_threshold = True
+    return (over_threshold, ratio_result)
+
+
 wb = openpyxl.load_workbook("check_target.xlsx") # Excelファイルの読み込み
 
 sheet_names = wb.sheetnames # シート名一覧取得
@@ -9,11 +17,15 @@ sheet_names = wb.sheetnames # シート名一覧取得
 ws_target = wb['target'] # シートの取得
 ws_list = wb['list'] # シートの取得
 
+results = {}
+
 row_target = 1
 while True:
     value_target = ws_target.cell(row=row_target, column=1).value
     if value_target == None:
         break
+
+    max_threshold = 0
 
     row_list = 1
     while True:
@@ -21,8 +33,17 @@ while True:
         if value_list == None:
             break
 
-        print(value_list)
+        value_target = value_target.rstrip()
+        value_list = value_list.rstrip()
+
+        over_threshold, threshold = check_similarity_threshold(value_target, value_list)
+        if max_threshold < threshold:
+            max_threshold = threshold
+            results[value_target] = (value_list, threshold)
+
         row_list += 1
 
-    print(value_target)
     row_target += 1
+
+for r in results:
+    print(r)
